becf8bf01c9ba73a1de12d009f07a7506b846c84
AliWoWolx <50416155+AliWoWolx@users.noreply.github.com>
2020-06-04 13:23:23 +0100

    (added which day of the week you were born)

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,20 @@
 # Copyright (C). No permission granted. All mine.
+import datetime
+import calendar
+
 name = input("Please enter your name: ")
 
 print(f"Hello, {name}!")
 print('Have a nice day')
+
+try:
+    date_entry = input('Enter your date of birth in dd/mm/yyyy format: ')
+    day, month, year  = map(int, date_entry.split('/'))
+    dob = datetime.date(year, month,day )
+    print(dob)
+    i=dob.weekday()
+    #print(i)
+except (ValueError):
+    "Did you use the expected format?"
+
+print("You were born on a ", calendar.day_name[i],"!")
