d8e3a1fd1d94bcd580be492adec12ff1b10c16ef
Golpette <stevenjamescourt@gmail.com>
2020-06-29 18:30:53 +0100

    add requirements for demo

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,21 @@
 # Copyright (C). No permission granted. All mine.
 #Pointless edit by SG 11th June2020 to test out Github
-name = input("Please enter your name: ")
 
-print(f"Hello, {name}!")
+import numpy
+import easygui
 
-weather = input ("What is the weather like today?")
 
-if (weather == 'rain'): print('Oh well better luck tomorrow')
-else: print("Enjoy it whilst t lasts!")    
+print()
+print("Supercalifragilisticexpialidocious!")
+print()
+
+
+#name = input("Please enter your name: ")
+#
+#print(f"Hello, {name}!")
+#
+#weather = input ("What is the weather like today?")
+#
+#if (weather == 'rain'): print('Oh well better luck tomorrow')
+#else: print("Enjoy it whilst t lasts!")    
 
